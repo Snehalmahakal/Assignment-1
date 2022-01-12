84167911749876efa7c671d8fb9fc31c989ac04a
Snehalmahakal <97621253+Snehalmahakal@users.noreply.github.com>
2022-01-12 22:35:13 +0530

    Add files via upload

---
+++ pro1.java
@@ -0,0 +1,7 @@
+class pro1
+{
+public static void main(String args[])
+{
+System.out.println("Name=Snehal Mahakal \nAdd=Jaigad Ratnagiri \nNo=9168509414 \nEmail=snehalmahakal11@gmail.com");
+}
+}
---
+++ pro2.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+class pro2
+{
+public static void main(String args[])
+{
+//int num1,num2,sum;
+/*Scanner sc=new Scanner(System.in);
+System.out.println("Enter First Number");
+num1=sc.nextInt();
+System.out.println("Enter Second Number");
+num2=sc.nextInt();
+sum=num1+num2;
+System.out.println("The addition is="+sum);
+*/
+
+
+//int num1,num2,Multiply;
+/*Scanner sc=new Scanner(System.in);
+System.out.println("Enter First Number");
+num1=sc.nextInt();
+System.out.println("Enter Second Number");
+num2=sc.nextInt();
+Multiply=num1*num2;
+System.out.println("The Multiplication is="+Multiply);
+*/
+
+//int num1,num2,Minus;
+/*Scanner sc=new Scanner(System.in);
+System.out.println("Enter First Number");
+num1=sc.nextInt();
+System.out.println("Enter Second Number");
+num2=sc.nextInt();
+Minus=num1-num2;
+System.out.println("The substraction is="+Minus);
+*/
+
+//int num1,num2,Divide;
+/*Scanner sc=new Scanner(System.in);
+System.out.println("Enter First Number");
+num1=sc.nextInt();
+System.out.println("Enter Second Number");
+num2=sc.nextInt();
+Divide=num1/num2;
+System.out.println("The Division is="+Divide);
+*/
+
+int num1,num2,Remainder;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter First Number");
+num1=sc.nextInt();
+System.out.println("Enter Second Number");
+num2=sc.nextInt();
+Remainder=num1%num2;
+System.out.println("The Remainder is="+Remainder);
+
+}
+}
+
